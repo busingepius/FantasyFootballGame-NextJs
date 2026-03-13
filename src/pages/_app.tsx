287e1c9ff000c0ae7b98f151a35ed098fb159a5b
pius businge <piusbusinge@piuss-Mac-Studio.local>
2026-03-12 19:48:24 -0500

    Add viewport meta tag for mobile responsiveness

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import { store, persistor } from '@/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { AuthWrapper } from "@/hocs/AuthWrapper";
 import AppInitializer from '@/init/AppInitializer';
+import Head from 'next/head';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -23,28 +24,33 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
 
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <AppInitializer />
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            style: {
-              borderRadius: '8px',
-              background: '#1f2937',
-              color: '#fff',
-            },
-          }}
-        />
-        {Component.requiresAuth
-          ? getLayout(
-              <AuthWrapper>
-                <Component {...pageProps} />
-              </AuthWrapper>
-            )
-          : getLayout(<Component {...pageProps} />)
-        }
-      </PersistGate>
-    </Provider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppInitializer />
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              style: {
+                borderRadius: '8px',
+                background: '#1f2937',
+                color: '#fff',
+              },
+            }}
+          />
+          {Component.requiresAuth
+            ? getLayout(
+                <AuthWrapper>
+                  <Component {...pageProps} />
+                </AuthWrapper>
+              )
+            : getLayout(<Component {...pageProps} />)
+          }
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
